734ab49c11ba1b3875f495fbb855035c96c17430
Ofir Dahan <77748302+ofirdahan750@users.noreply.github.com>
2025-12-19 12:36:10 +0200

    add q&a - handler for no q mark

--- libs/server/bot-engine/src/register-socket/register-socket-handlers.ts
+++ libs/server/bot-engine/src/register-socket/register-socket-handlers.ts
@@ -67,7 +67,9 @@ export const registerSocketHandlers = (io: Server): void => {
         ...incoming,
         id: incoming.id || randomUUID(),
         timestamp:
-          typeof incoming.timestamp === 'number' ? incoming.timestamp : Date.now(),
+          typeof incoming.timestamp === 'number'
+            ? incoming.timestamp
+            : Date.now(),
       };
 
       pushToHistory(roomId, serverMsg);
