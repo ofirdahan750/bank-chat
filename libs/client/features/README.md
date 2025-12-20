be2a3e01447e21a3ce13aeb4eae7c5ba68e23b4c
Ofir Dahan <77748302+ofirdahan750@users.noreply.github.com>
2025-12-20 16:00:34 +0200

    refactor the project

--- libs/client/features/README.md
+++
@@ -1,7 +0,0 @@
-# client-features
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test client-features` to execute the unit tests.
--- libs/client/features/src/lib/client-features/client-features.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-client-features',
-  imports: [],
-  templateUrl: './client-features.html',
-  styleUrl: './client-features.scss',
-})
-export class ClientFeatures {}
